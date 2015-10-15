d84dfa4eb2585d325baf3a26e9aa8ec3507390f3
Arul15 <arulpandiyan@Sybrants-Mac-mini-2.local>
2015-10-15 12:23:35 +0530

    Initial commit multiple request to afnetworking
    Initial commit multiple request to afnetworking

---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AFNetworkSample
+//
+//  Created by Arulpandiyan on 14/10/15.
+//  Copyright (c) 2015 sybrant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ViewController.m
@@ -0,0 +1,93 @@
+//
+//  ViewController.m
+//  AFNetworkSample
+//
+//  Created by Arulpandiyan on 14/10/15.
+//  Copyright (c) 2015 sybrant. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "UIImageView+AFNetworking.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"come here or not");
+    
+    
+   ////// Multiple Request//////
+    NSURL *url = [NSURL URLWithString:@"URL HERE"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+//    [httpClient registerHTTPOperationClass:[AFHTTPRequestOperation class]];
+    [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        // Print the response body in text
+        NSLog(@"Response: %@", [[NSString alloc] initWithData:responseObject encoding:NSUTF8StringEncoding]);
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"Error: %@", error);
+    }];
+    
+    
+    NSURL *url1 = [NSURL URLWithString:@"URL HERE"];
+    NSURLRequest *request1 = [NSURLRequest requestWithURL:url1];
+    AFHTTPRequestOperation *operation1 = [[AFHTTPRequestOperation alloc] initWithRequest:request1];
+    //    [httpClient registerHTTPOperationClass:[AFHTTPRequestOperation class]];
+    [operation1 setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        // Print the response body in text
+        NSLog(@"Response22222222222: %@", responseObject);
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"Error: %@", error);
+    }];
+
+    // Add the operation to a queue
+    // It will start once added
+    NSOperationQueue *operationQueue = [[NSOperationQueue alloc] init];
+    [operationQueue setMaxConcurrentOperationCount:4];
+    [operationQueue addOperations:@[operation, operation1] waitUntilFinished:NO];
+
+    
+//    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+//    [manager GET:@"URL HERE" parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+//        NSLog(@"JSON: %@", responseObject);
+//    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+//        NSLog(@"Error: %@", error);
+//    }];
+    
+//    NSString *string = [NSString stringWithFormat:@"http://www.raywenderlich.com/demos/weather_sample/weather.php?format=json"];
+//    NSURL *url = [NSURL URLWithString:string];
+//    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+//    
+//    // 2
+//    AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+//    operation.responseSerializer = [AFJSONResponseSerializer serializer];
+//    NSLog(@"request is %@",request);
+//    [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+//        NSLog(@"the formate of 1111 %@",(NSDictionary *)responseObject);
+//        // 3
+//        
+//    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+//        // 4
+//        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error Retrieving Weather"
+//                                                            message:[error localizedDescription]
+//                                                           delegate:nil
+//                                                  cancelButtonTitle:@"Ok"
+//                                                  otherButtonTitles:nil];
+//        [alertView show];
+//    }];
+    
+    // 5
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
